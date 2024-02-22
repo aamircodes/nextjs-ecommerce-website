69745b136db35ced0ab160c4e4b984f56b8c2678
aamir <aamirp1@outlook.com>
2024-02-22 21:25:15 +0000

    Add product list page

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [{ hostname: "images.unsplash.com/" }],
-  },
-};
-
-export default nextConfig;
